255ebfd85f3007af4255f0f41fa4f362831b051b
gmahesa <galangmahesa99@gmail.com>
2023-05-25 11:03:34 +0800

    test fix reveal password

--- scripts/revealPassword.js
+++ scripts/revealPassword.js
@@ -1,10 +1,17 @@
 $(document).ready(function () {
   // REQUIRING "appUrl" VARIABLE
   var inputPassword = $(".revealPassword input");
-  var inputPasswordIcon = $(".revealPassword div img");
-  var openEye = "images/svgs/ic_eye.svg";
-  var closeEye = "images/svgs/ic_eye-off.svg";
+
+  var inputPasswordIcon = $(".revealPassword div .icon");
+  var openEye = "../../images/svgs/ic_eye.svg";
+  var closeEye = "../../images/svgs/ic_eye-off.svg";
+
+  var inputPasswordIcon1 = $(".revealPassword div .icon1");
+  var openEye1 = "images/svgs/ic_eye.svg";
+  var closeEye1 = "images/svgs/ic_eye-off.svg";
+
   inputPasswordIcon.hide();
+  inputPasswordIcon1.hide();
 
   $(() => {
     inputPassword.on("keyup", (e) => {
@@ -26,6 +33,19 @@ $(document).ready(function () {
       } else {
         currentPasswordField.setAttribute("type", "password");
         currentElementIcon.setAttribute("src", openEye);
+      }
+    });
+
+    inputPasswordIcon1.on("click", (e) => {
+      var currentElementIcon = e.currentTarget;
+      var currentPasswordField = currentElementIcon.parentElement.previousElementSibling;
+      if (currentPasswordField.getAttribute("type") == "password") {
+        currentPasswordField.setAttribute("type", "text");
+        currentElementIcon.setAttribute("src", closeEye1);
+        // console.log(currentPasswordField)
+      } else {
+        currentPasswordField.setAttribute("type", "password");
+        currentElementIcon.setAttribute("src", openEye1);
       }
     });
   });
